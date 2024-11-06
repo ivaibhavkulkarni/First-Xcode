a3399135a553a2df72f4abcdb64bbd1ee23f885f
vaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-11-06 16:16:48 +0530

    Initial Commit

---
+++ First Xcode/First_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_XcodeApp.swift
+//  First Xcode
+//
+//  Created by Vaibhav kulkarni on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct First_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
